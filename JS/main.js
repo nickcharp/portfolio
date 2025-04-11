cc0108367959cd143b214a1d57fdb3c67a085f88
Nick Charpentier <39897122+nickcharp@users.noreply.github.com>
2025-04-11 01:02:32 -0600

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
-    // Collapse navbar on nav-link click
-    $('.navbar-nav .nav-link').on('click', function() {
+    // Collapse navbar on nav-link click, excluding dropdown-toggle
+    $('.navbar-nav .nav-link:not(.dropdown-toggle)').on('click', function() {
         if ($('.navbar-toggler').is(':visible')) {
             $('.navbar-collapse').collapse('hide');
         }
     });
+
     // Collapse navbar on dropdown-item click
     $('.dropdown-item').on('click', function() {
         if ($('.navbar-toggler').is(':visible')) {
             $('.navbar-collapse').collapse('hide');
+        }
+    });
+
+    // Prevent dropdown-toggle from closing navbar immediately
+    $('.navbar-nav .dropdown-toggle').on('click', function(e) {
+        if ($('.navbar-toggler').is(':visible')) {
+            e.stopPropagation(); // Stop event from triggering navbar collapse
         }
     });
 });
