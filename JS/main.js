b317551024f7bd9bda43166ac2ca330aabbf1b73
Nick Charpentier <39897122+nickcharp@users.noreply.github.com>
2025-04-11 01:10:09 -0600

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-    // Collapse navbar on non-dropdown nav-link clicks
+    // Handle non-dropdown nav-link clicks to collapse navbar
     $('.navbar-nav .nav-link').not('.dropdown-toggle').on('click', function() {
         if ($('.navbar-toggler').is(':visible')) {
             $('.navbar-collapse').collapse('hide');
+        }
+    });
+
+    // Prevent navbar collapse and ensure dropdown opens on Projects click
+    $('.navbar-nav .dropdown-toggle').on('click', function(e) {
+        if ($('.navbar-toggler').is(':visible')) {
+            e.preventDefault(); // Stop default collapse behavior
+            e.stopImmediatePropagation(); // Stop all event propagation
+            const $this = $(this);
+            const $dropdown = $this.next('.dropdown-menu');
+            
+            // If dropdown isn’t open, show it manually
+            if (!$dropdown.hasClass('show')) {
+                $this.dropdown('show');
+            }
         }
     });
 
