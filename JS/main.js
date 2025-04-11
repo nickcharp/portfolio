80d487d9f07069864a247fe14a4248abc54b8162
Nick Charpentier <39897122+nickcharp@users.noreply.github.com>
2025-04-11 01:08:44 -0600

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-    // Collapse navbar only when a dropdown-item is clicked
+    // Collapse navbar on non-dropdown nav-link clicks
+    $('.navbar-nav .nav-link').not('.dropdown-toggle').on('click', function() {
+        if ($('.navbar-toggler').is(':visible')) {
+            $('.navbar-collapse').collapse('hide');
+        }
+    });
+
+    // Collapse navbar when a dropdown-item is clicked
     $('.dropdown-item').on('click', function() {
         if ($('.navbar-toggler').is(':visible')) {
             $('.navbar-collapse').collapse('hide');
