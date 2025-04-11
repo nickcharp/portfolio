d39fa24d992d32331fe82bb693048574e1a519ee
Nick Charpentier <39897122+nickcharp@users.noreply.github.com>
2025-04-11 00:48:44 -0600

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
+    // Collapse navbar on nav-link click
     $('.navbar-nav .nav-link').on('click', function() {
+        if ($('.navbar-toggler').is(':visible')) {
+            $('.navbar-collapse').collapse('hide');
+        }
+    });
+    // Collapse navbar on dropdown-item click
+    $('.dropdown-item').on('click', function() {
         if ($('.navbar-toggler').is(':visible')) {
             $('.navbar-collapse').collapse('hide');
         }
