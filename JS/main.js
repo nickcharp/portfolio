5752ad94fd0bfeaaf6304aebc3cc32107916230f
Nick Charpentier <39897122+nickcharp@users.noreply.github.com>
2025-04-11 01:05:18 -0600

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,34 @@
 $(document).ready(function() {
-    // Collapse navbar on nav-link click, excluding dropdown-toggle
+    // Collapse navbar on non-dropdown nav-link clicks
     $('.navbar-nav .nav-link:not(.dropdown-toggle)').on('click', function() {
         if ($('.navbar-toggler').is(':visible')) {
             $('.navbar-collapse').collapse('hide');
         }
     });
 
-    // Collapse navbar on dropdown-item click
+    // Collapse navbar when a dropdown-item is clicked
     $('.dropdown-item').on('click', function() {
         if ($('.navbar-toggler').is(':visible')) {
             $('.navbar-collapse').collapse('hide');
         }
     });
 
-    // Prevent dropdown-toggle from closing navbar immediately
+    // Prevent navbar collapse when dropdown-toggle is clicked on mobile
     $('.navbar-nav .dropdown-toggle').on('click', function(e) {
         if ($('.navbar-toggler').is(':visible')) {
-            e.stopPropagation(); // Stop event from triggering navbar collapse
+            e.preventDefault(); // Prevent default Bootstrap collapse behavior
+            e.stopPropagation(); // Stop event bubbling
+            // Manually toggle dropdown if not already open
+            if (!$(this).parent().hasClass('show')) {
+                $(this).dropdown('toggle');
+            }
+        }
+    });
+
+    // Keep navbar open when dropdown is shown
+    $('.dropdown').on('shown.bs.dropdown', function() {
+        if ($('.navbar-toggler').is(':visible')) {
+            $('.navbar-collapse').collapse('show'); // Ensure navbar stays open
         }
     });
 });
